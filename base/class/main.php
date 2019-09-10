ca4505624797bee524e53f30e8afeb4f89111429
TOMCO984 <tomco984@live.fr>
2019-09-10 11:28:38 +0200

    edit

---
+++ base/class/main.php
@@ -0,0 +1,27 @@
+<?php
+/* Classe principale
+ * Auteur : COCHE Thomas <contact@thomas-coche.fr>
+ * Version : 0.0.5 (13/05/2019) -- PHP 7.2
+ * Description Complète :
+ *  - Inclusion de toutes les autres classes et des librairies
+ *  - Initialisation des sessions et de la base de donnée
+ */
+ session_start();
+
+ require_once(__DIR__ . "/settings.php");
+ Class Main {
+
+   public static function Database() {
+     return $Database = new PDO(
+       "mysql:host=".Settings::dbHost.
+       ";dbname=".Settings::dbName,
+       Settings::dbUser,
+       Settings::dbPassword,
+       array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
+     );
+   }
+
+ }
+
+# require(__DIR__ . "/page.php");
+?>
