d83fc428befbbd4f7ae9dd2ef87fbfd838cc70eb
Chihchine <42942052+Chihchine@users.noreply.github.com>
2019-09-10 11:08:27 +0200

    ProfilPHP start

---
+++ profil.php
@@ -0,0 +1,14 @@
+<html> 
+<head>
+	<link rel="stylesheet" href = "css/profil.css" /> 
+	<link rel="icon" href=""/>
+
+</head>
+<body>
+	<div id="presentation"> 
+		<img id="profil-image" src="images/profils/profiltest.png" /> 
+		<h4>Nom: Reynard </h4> <!-- PHP "Nom" + Data.Name--> 
+		<h4> Prénom: Aurélien </h4>
+	</div>
+</body>
+</html>
