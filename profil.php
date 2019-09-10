64e9320efb4f5286ca0a4075045c062aa771c4a5
Chihchine <42942052+Chihchine@users.noreply.github.com>
2019-09-10 12:48:39 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -1,4 +1,3 @@
-<<<<<<< current
 <?php
 require("base/include/header.php");
 
@@ -10,66 +9,24 @@ $result->execute(array($id));
 $userexist = $result->rowCount();
 if($userexist == 1){
 	$userinfo = $result->fetch();
-<<<<<<< HEAD
-	echo $userinfo['NOM'];
-} else
-=======
 	echo $userinfo['id'];
 } else 
->>>>>>> 2a0f03d942ced6f6f2717e35ad13bcc16998fec7
 {
 	echo "Marche pas";
 }
-?>
-chichine's
-<html>
+?> 
+
+<html> 
 <head>
-	<link rel="stylesheet" href = "base/css/profil.css" />
+	<link rel="stylesheet" href = "base/css/profil.css" /> 
 	<link rel="icon" href=""/>
 
 </head>
 <body>
-	<div id="presentation">
-		<img id="profil-image" src="images/profils/profiltest.png" />
-		<h4>Nom: Reynard </h4> <!-- PHP "Nom" + Data.Name-->
+	<div id="presentation"> 
+		<img id="profil-image" src="images/profils/profiltest.png" /> 
+		<h4>Nom: Reynard </h4> <!-- PHP "Nom" + Data.Name--> 
 		<h4> Prénom: Aurélien </h4>
 	</div>
 </body>
 </html>
-=======
-<?php
-require("base/include/header.php");
-
-//$request = Main::Database()->prepare("SELECT * FROM UTILISATEURS WHERE ID = :id");
-//$request->execute(['id' => "1"]);
-
-//$request = Main:DataBase()->prepare("SELECT * FROM UTILISATEURS WHERE ID = :id");
-//$request->execute(["id" => 1])
-
-
-$result = Main::DataBase()->prepare("SELECT * FROM UTILISATEURS");
-//$result->execute(["id" => 1]);
-
-//echo $result["id"];
-
-foreach($result as $utilisateur)
-{
-	echo $utilisateur["id"];
-}
-?>
-
-<html>
-<head>
-	<link rel="stylesheet" href = "base/css/profil.css" />
-	<link rel="icon" href=""/>
-
-</head>
-<body>
-	<div id="presentation">
-		<img id="profil-image" src="images/profils/profiltest.png" />
-		<h4>Nom: Reynard </h4> <!-- PHP "Nom" + Data.Name-->
-		<h4> Prénom: Aurélien </h4>
-	</div>
-</body>
-</html>
->>>>>>> before discard
