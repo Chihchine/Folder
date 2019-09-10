f792c4430bfc5369ffa56dccb3aa70ea80721888
TOMCO984 <tomco984@live.fr>
2019-09-10 16:54:24 +0200

    update

--- groupes.php
+++ groupes.php
@@ -22,21 +22,17 @@ require("base/include/header.php");
             <label for="clubDesc">Description du groupe</label>
             <textarea class="col-12" id="clubDesc" name="clubDesc" rows="3" placeholder="Une description simple du club avec ses objectifs, pour qui, quand, etc..."></textarea>
           </div>
-          <div class="input-group image-preview">
-                  <input type="text" class="form-control image-preview-filename" disabled="disabled">
-                  <span class="input-group-btn">
-                      <!-- image-preview-clear button -->
-                      <button type="button" class="btn btn-default image-preview-clear" style="display:none;">
-                          <span class="glyphicon glyphicon-remove"></span> Annuler
-                      </button>
-                      <!-- image-preview-input -->
-                      <div class="btn btn-default image-preview-input">
-                          <span class="glyphicon glyphicon-folder-open"></span>
-                          <span class="image-preview-input-title">Choisissez une image</span>
-                          <input type="file" accept="image/png, image/jpeg, image/gif" name="input-file-preview"/> <!-- rename it -->
-                      </div>
-                  </span>
-              </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+            </div>
+            <div class="custom-file">
+              <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+              <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+            <div class="custom-file">
+              <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" value="Choisissez une image">
+            </div>
+          </div>
         </form>
       </div>
       <div class="modal-footer">
