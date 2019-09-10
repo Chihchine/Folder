acef77b7dfcb8d3bcf1ebc7d4c3b25124bb3e10f
TOMCO984 <tomco984@live.fr>
2019-09-10 12:45:38 +0200

    test

--- profil.php
+++ profil.php
@@ -1,3 +1,4 @@
+<<<<<<< current
 <?php
 require("base/include/header.php");
 
@@ -10,23 +11,60 @@ $userexist = $result->rowCount();
 if($userexist == 1){
 	$userinfo = $result->fetch();
 	echo $userinfo['NOM'];
-} else 
+} else
 {
 	echo "Marche pas";
 }
-?> 
-
-<html> 
+?>
+chichine's
+<html>
 <head>
-	<link rel="stylesheet" href = "base/css/profil.css" /> 
+	<link rel="stylesheet" href = "base/css/profil.css" />
 	<link rel="icon" href=""/>
 
 </head>
 <body>
-	<div id="presentation"> 
-		<img id="profil-image" src="images/profils/profiltest.png" /> 
-		<h4>Nom: Reynard </h4> <!-- PHP "Nom" + Data.Name--> 
+	<div id="presentation">
+		<img id="profil-image" src="images/profils/profiltest.png" />
+		<h4>Nom: Reynard </h4> <!-- PHP "Nom" + Data.Name-->
 		<h4> Prénom: Aurélien </h4>
 	</div>
 </body>
 </html>
+=======
+<?php
+require("base/include/header.php");
+
+//$request = Main::Database()->prepare("SELECT * FROM UTILISATEURS WHERE ID = :id");
+//$request->execute(['id' => "1"]);
+
+//$request = Main:DataBase()->prepare("SELECT * FROM UTILISATEURS WHERE ID = :id");
+//$request->execute(["id" => 1])
+
+
+$result = Main::DataBase()->prepare("SELECT * FROM UTILISATEURS");
+//$result->execute(["id" => 1]);
+
+//echo $result["id"];
+
+foreach($result as $utilisateur)
+{
+	echo $utilisateur["id"];
+}
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href = "base/css/profil.css" />
+	<link rel="icon" href=""/>
+
+</head>
+<body>
+	<div id="presentation">
+		<img id="profil-image" src="images/profils/profiltest.png" />
+		<h4>Nom: Reynard </h4> <!-- PHP "Nom" + Data.Name-->
+		<h4> Prénom: Aurélien </h4>
+	</div>
+</body>
+</html>
+>>>>>>> before discard
