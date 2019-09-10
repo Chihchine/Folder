d0f22b24c5d60356d39007d79cec05a169f4fe0f
Chihchine <42942052+Chihchine@users.noreply.github.com>
2019-09-10 12:21:52 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -9,7 +9,7 @@ require("base/class/main.php");
 
 
 $result = Main::DataBase()->prepare("SELECT * FROM UTILISATEURS WHERE ID = id");
-$result->execute(["id" => 1]);
+//$result->execute(["id" => 1]);
 
 //echo $result["id"];
 
