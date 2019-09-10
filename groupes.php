fe7a6cd5bd2773326ca2e0a10d7192026ce7fce6
TOMCO984 <tomco984@live.fr>
2019-09-10 22:59:58 +0200

    update

--- groupes.php
+++ groupes.php
@@ -23,21 +23,6 @@ require("base/include/header.php");
             <textarea class="col-12" id="clubDesc" name="clubDesc" rows="3" placeholder="Une description simple du club avec ses objectifs, pour qui, quand, etc..."></textarea>
           </div>
         </form>
-        <div class="input-group image-preview">
-          <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- don't give a name === doesn't send on POST/GET -->
-          <span class="input-group-btn">
-            <!-- image-preview-clear button -->
-            <button type="button" class="btn btn-default image-preview-clear" style="display:none;">
-              <span class="glyphicon glyphicon-remove"></span> Clear
-            </button>
-            <!-- image-preview-input -->
-            <div class="btn btn-default image-preview-input">
-              <span class="glyphicon glyphicon-folder-open"></span>
-              <span class="image-preview-input-title">Browse</span>
-              <input type="file" accept="image/png, image/jpeg, image/gif" name="input-file-preview"/> <!-- rename it -->
-            </div>
-          </span>
-        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
@@ -55,6 +40,22 @@ require("base/include/header.php");
       <li class="breadcrumb-item active" aria-current="page">Clubs</li>
     </ol>
   </nav>
+</div>
+
+<div class="input-group image-preview">
+  <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- don't give a name === doesn't send on POST/GET -->
+  <span class="input-group-btn">
+    <!-- image-preview-clear button -->
+    <button type="button" class="btn btn-default image-preview-clear" style="display:none;">
+      <span class="glyphicon glyphicon-remove"></span> Clear
+    </button>
+    <!-- image-preview-input -->
+    <div class="btn btn-default image-preview-input">
+      <span class="glyphicon glyphicon-folder-open"></span>
+      <span class="image-preview-input-title">Browse</span>
+      <input type="file" accept="image/png, image/jpeg, image/gif" name="input-file-preview"/> <!-- rename it -->
+    </div>
+  </span>
 </div>
 
 <div class="container">
