c1b849974efbd235545618f6f89185a6772bfb4b
gauthiermairot <55086155+gauthiermairot@users.noreply.github.com>
2019-09-10 15:59:39 +0200

    CORRECTION

--- base/class/inscription.php
+++ base/class/inscription.php
@@ -2,13 +2,13 @@
 Class Inscription {
 
   public function inscription($nom, $prenom, $mail, $mdp, $ecole, $promotion) {
-    $request = Main::Database()->prepare('INSERT INTO utilisateurs(NOM, PRENOM, MAIL, MDP, ECOLE, PROMOTION) VALUES (?, ?, ?, ?, ?, ?)');
+    $request = Main::Database()->prepare('INSERT INTO UTILISATEURS(NOM, PRENOM, MAIL, MDP, ECOLE, PROMOTION) VALUES (?, ?, ?, ?, ?, ?)');
     $result = $request->execute(array($nom, $prenom, $mail, $mdp, $ecole, $promotion));
     return $result;
   }
 
   public function mailExists($mail) {
-    $request = Main::Database()->prepare('SELECT MAIL FROM utilisateurs WHERE MAIL = ?');
+    $request = Main::Database()->prepare('SELECT MAIL FROM UTILISATEURS WHERE MAIL = ?');
     $request->execute(array($mail));
     $result = $request->fetch();
     return $result;
