24a88c35fc6cf2b01156212c44482b8876728c87
gauthiermairot <55086155+gauthiermairot@users.noreply.github.com>
2019-09-10 16:01:54 +0200

    correction

--- base/class/inscription.php
+++ base/class/inscription.php
@@ -1,13 +1,13 @@
 <?php
 Class Inscription {
 
-  public function inscription($nom, $prenom, $mail, $mdp, $ecole, $promotion) {
+  public static function inscription($nom, $prenom, $mail, $mdp, $ecole, $promotion) {
     $request = Main::Database()->prepare('INSERT INTO UTILISATEURS(NOM, PRENOM, MAIL, MDP, ECOLE, PROMOTION) VALUES (?, ?, ?, ?, ?, ?)');
     $result = $request->execute(array($nom, $prenom, $mail, $mdp, $ecole, $promotion));
     return $result;
   }
 
-  public function mailExists($mail) {
+  public static function mailExists($mail) {
     $request = Main::Database()->prepare('SELECT MAIL FROM UTILISATEURS WHERE MAIL = ?');
     $request->execute(array($mail));
     $result = $request->fetch();
