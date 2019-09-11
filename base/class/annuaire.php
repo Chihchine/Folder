d5cf68e645498cb5a0491ee2e2dd1ea329490c51
gauthiermairot <55086155+gauthiermairot@users.noreply.github.com>
2019-09-11 13:39:38 +0200

    fix

--- base/class/annuaire.php
+++ base/class/annuaire.php
@@ -2,8 +2,8 @@
 Class Annuaire {
   public static function displayUsers() {
     $request = Main::Database()->prepare('SELECT NOM, PRENOM, ECOLE, PROMOTION FROM UTILISATEURS');
-    $result = $request->execute();
-    return $result;
+    $request->execute();
+    return $request;
   }
 }
 
