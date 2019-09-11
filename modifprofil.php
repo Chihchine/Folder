aa7db2d590678fd9558dbf7f246accf37f59ace7
Chihchine <42942052+Chihchine@users.noreply.github.com>
2019-09-11 12:36:04 +0200

    Update modifprofil.php

--- modifprofil.php
+++ modifprofil.php
@@ -133,7 +133,71 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
 	                <th class="colonnes-gauches" scope="row">Oof</th>
 	                <td class="colonnes-droites">X commentaires</td>
 	              </tr>
-	            </tbody>
+
+
+
+	              	<form method="post" class="needs-validation form-signin" novalidate>
+					      <img class="mb-4">
+					      <h1 class="h3 mb-3 font-weight-normal">Inscription</h1>
+					      <div class="form">
+					        <div class="form-row">
+					          <div class="form-group col-md-6">
+					            <label for="">Prénom</label>
+					            <input class="form-control" type="text" name="prenom"  placeholder="Prénom" required>
+					            <div class="invalid-feedback">Veuillez-renseigner votre prénom.</div>
+					          </div>
+					          <div class="form-group col-md-6">
+					            <label for="">Nom</label>
+					            <input class="form-control" type="text" name="nom"  placeholder="Nom" required>
+					            <div class="invalid-feedback">Veuillez-renseigner votre nom.</div>
+					          </div>
+					        </div>
+					        <div class="form-row">
+					          <div class="form-group col-md-4">
+					            <label for="">Email</label>
+					            <input class="form-control" type="email" name="mail"  placeholder="Email de votre école" required>
+					            <div class="invalid-feedback">Veuillez-renseigner un email valide.</div>
+					          </div>
+					          <div class="form-group col-md-4">
+					            <label for="">Mot de passe</label>
+					            <input class="form-control" type="password" name="mdp"  placeholder="*********" required>
+					            <div class="invalid-feedback">Minimum 6 caractères.</div>
+					          </div>
+					          <div class="form-group col-md-4">
+					            <label for="">Confirmer</label>
+					            <input class="form-control" type="password" name="confirmMdp" placeholder="*********" required>
+					            <div class="invalid-feedback">Veuillez confirmer votre mot de passe.</div>
+					          </div>
+					        </div>
+					        <div class="form-row">
+					          <div class="form-group col-md-6">
+					            <label for="">Ecole</label>
+					            <select class="form-control" name="ecole">
+					              <option value="EPSI">EPSI</option>
+					              <option value="IDRAC">IDRAC</option>
+					              <option value="WIS">WIZ</option>
+					            </select>
+					          </div>
+					          <div class="form-group col-md-6">
+					            <label for="">Niveau d'étude</label>
+					            <select class="form-control" name="promotion">
+					              <option value="B1">1ère année</option>
+					              <option value="B2">2ème année</option>
+					              <option value="B3">3ème année</option>
+					              <option value="M1">4ème année</option>
+					              <option value="M2">5ème année</option>
+					            </select>
+					          </div>
+					        </div>
+					      </div>
+					      <a href="connexion.php">Vous avez déjà un compte ?</a>
+					      <button class="btn btn-lg btn-primary btn-block" type="submit" name="btnInscrire">S'inscrire</button>
+					      <p class="text-muted">&copy; 2019 - Nom du site</p>
+					    </form>
+
+
+
+	            </body>
 	          </table>
 	        </div>
 	      </div>
