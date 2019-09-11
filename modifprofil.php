7524f81712cce96d38b45bfbdbfe7d7d17f0644d
Chihchine <42942052+Chihchine@users.noreply.github.com>
2019-09-11 13:43:05 +0200

    Update modifprofil.php

--- modifprofil.php
+++ modifprofil.php
@@ -135,28 +135,22 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
 					            <input class="form-control" type="email" name="newmail"  placeholder="<?php echo $userinfo["MAIL"] ?>" required>
 					            <div class="invalid-feedback">Veuillez renseigner un email valide.</div>
 					          </div>
-					        </div>
-					        </br>
 					          <div class="form-group col-md-4">
 					            <label for="">Presentation</label>
 					            <input class="form-control" type="text" name="pres"  placeholder="<?php echo $userinfo["PRESENTATION"] ?>" required>
 					            <div class="invalid-feedback">Ne pas dépasser 150 caractères</div>
 					          </div>
-					        </br>
 					          <div class="form-group col-md-4">
 					            <label for="">Nouveau Mot de passe</label>
 					            <input class="form-control" type="password" name="newmdp"  placeholder="*********" required>
 					            <div class="invalid-feedback">Minimum 6 caractères.</div>
 					          </div>
-					  		</br>
-					  		<div>
 					          <div class="form-group col-md-4">
 					            <label for="">Confirmer</label>
 					            <input class="form-control" type="password" name="confirmMdp" placeholder="*********" required>
 					            <div class="invalid-feedback">Veuillez confirmer votre nouveau mot de passe.</div>
 					          </div>
 					        </div>
-					    	</br>
 					        <div class="form-row">
 					          <div class="form-group col-md-6">
 					            <label for="">Nouvelle Ecole</label>
@@ -166,8 +160,6 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
 					              <option value="WIS">WIS</option>
 					            </select>
 					          </div>
-					        </div>
-					        </br>
 					          <div class="form-group col-md-6">
 					            <label for="">Niveau d'étude</label>
 					            <select class="form-control" name="newpromotion">
@@ -178,6 +170,7 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
 					              <option value="M2">5ème année</option>
 					            </select>
 					          </div>
+					        </div>
 					      </div>
 					      <button class="btn btn-lg btn-primary btn-block" type="submit" name="btnInscrire">Sauvegarder</button>
 					      <p class="text-muted">&copy; 2019 - Nom du site</p>
