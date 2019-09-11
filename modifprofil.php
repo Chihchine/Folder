cd2b5921fd1a4cfe717e6e8638cb4b1ac5e129b9
Chihchine <42942052+Chihchine@users.noreply.github.com>
2019-09-11 14:24:24 +0200

    Update modifprofil.php

--- modifprofil.php
+++ modifprofil.php
@@ -87,6 +87,27 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
 	} else {
 		$profilimage = "images/basicprofil.png";
 	}
+
+
+	if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])) {
+	   $tailleMax = 2097152;
+	   $extensionsValides = array('jpg', 'jpeg', 'gif', 'png');
+	   if($_FILES['avatar']['size'] <= $tailleMax) {
+	      $extensionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
+	      if(in_array($extensionUpload, $extensionsValides)) {
+	         $chemin = "membres/avatars/".$_SESSION['id'].".".$extensionUpload;
+	         $resultat = move_uploaded_file($_FILES['avatar']['tmp_name'], $chemin);
+	         if($resultat) {
+	            $updateavatar = $bdd->prepare('UPDATE membres SET avatar = :avatar WHERE id = :id');
+	            $updateavatar->execute(array(
+	               'avatar' => $_SESSION['id'].".".$extensionUpload,
+	               'id' => $_SESSION['id']
+	               ));
+	            header('Location: profil.php?id='.$_SESSION['id']);
+	         	} 	
+	  	 	}
+		}
+	}
 ?>
 
 <html> 
@@ -176,6 +197,7 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
 					              <option value="M2">5ème année</option>
 					            </select>
 					          </div>
+					          <input type="file" name="avatar">
 					        </div>
 					      </div>
 					      <button class="btn btn-lg btn-primary btn-block" type="submit" name="btnInscrire">Sauvegarder</button>
