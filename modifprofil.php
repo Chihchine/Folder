b50658743cf7f3218f3d5052d8f2ff234925f190
Chihchine <42942052+Chihchine@users.noreply.github.com>
2019-09-11 12:23:28 +0200

    Update modifprofil.php

--- modifprofil.php
+++ modifprofil.php
@@ -43,28 +43,115 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
 	}
 ?>
 
-<html>
-<head>
-  <link rel="stylesheet" type="text/css" href="<?php echo Settings::sitePathRoot; ?>base/css/main.css"/>
-	<link rel="icon" href=""/>
+<html> 
+	<body>
+	    <!--<div class="fil_arianne container">
+	    <nav aria-label="breadcrumb">
+	        <ol class="breadcrumb">
+	            <li class="breadcrumb-item"><a href="?p=dashboard">Tableau de bord</a></li>
+	            <li class="breadcrumb-item active" aria-current="page">Mon profil</li>
+	        </ol>
+	    </nav>
+	</div>-->
 
-</head>
-<body>
-	<div id="presentation">
-		<img id="profil-image" src="images/uploads/<?php if($imageexist==1){echo $imageinfo['LIEN'];} ?>" />
-		<h4> Nom: <?php echo $userinfo['NOM']; ?> </h4>
-		<h4> Prénom: <?php echo $userinfo['PRENOM']; ?> </h4>
-		<h4> Ecole: <?php echo $userinfo['ECOLE']; ?> </h4>
-		<h4> Promotion: <?php echo $userinfo['PROMOTION']; ?> </h4>
+	<div class="container-fluid profil-content">
+	  <h1>Profil</h1>
+	  <div class="row">
+	    <div class="col-md-3">
+	      <div class="card">
+	        <div class="card-body card-body-left">
+	          <img class="rounded" height="400px" src="<?php echo $profilimage; ?>" alt="Image du compte" style="width:100%">
+	          <h5>
+	          </h5>
+	          <h6>
+	          </h6>
+	        </div>
+	      </div>
+	    </div>
+	    <div class="col-md-9">
+	      <div class="card">
+	        <div class="card-header">
+	          Vous regardez le profil de: <?php echo $userinfo['NOM'] . " " . $userinfo['PRENOM'] ?>
+	        </div>
+	        <div class="card-body card-body-right">
+	          <table class="table">
+	            <tbody>
+	              <tr>
+	                <th class="colonnes-gauches" scope="row">Identité</th>
+	                <td class="colonnes-droites">
+	                	<?php echo $userinfo['NOM'] . " " . $userinfo['PRENOM'] ?>
+	                </td>
+	              </tr>
+	              <tr>
+	                <th class="colonnes-gauches" scope="row">Ecole</th>
+	                <td class="colonnes-droites">
+	                	<?php echo $userinfo['ECOLE']; ?>
+	                </td>
+	              </tr>
+	              <tr>
+	                <th class="colonnes-gauches" scope="row">Date d'inscription</th>
+	                <td class="colonnes-droites">           </td>
+	              </tr>
+	              <tr>
+	                <th class="colonnes-gauches" scope="row">Groupes</th>
+	                <td class="colonnes-droites">
+	               		<?php
+	                		$membrede = Main::DataBase()->prepare("SELECT * FROM MEMBRES_GROUPE WHERE ID_UTILISATEUR = ?");
+	                		$membrede->execute(array($id));
+	                		$membredeinfo = $membrede->fetchAll();
+
+							//echo $membredeinfo['ID_GROUPE'];
+
+							foreach($membredeinfo as $valeur){
+								//echo $valeur["ID_GROUPE"];
+
+								$groupe = Main::DataBase()->prepare("SELECT * FROM GROUPES WHERE ID = ?");
+								$groupe->execute(array($valeur["ID_GROUPE"]));
+
+								$groupeinfo = $groupe->fetch();
+		
+								echo '<a href="'.'groupe.php?id=' .$valeur["ID_GROUPE"].'">' .$groupeinfo["NOM"]. '</a>';
+								echo "</br>";
+							}
+
+							//
+
+	                		/*foreach ($membredeinfo['ID_GROUPE'] as $key => $value) {
+	                			echo $key; echo $value;
+	                		}*/
+
+	                	?>
+
+
+
+
+
+
+
+	                </td>
+	              </tr>
+	              <tr>
+	                <th class="colonnes-gauches" scope="row">Promotion</th>
+	                <td class="colonnes-droites"> <?php echo $userinfo['PROMOTION']; ?> </td>
+	              </tr>
+	              <tr>
+	                <th class="colonnes-gauches" scope="row">Oof</th>
+	                <td class="colonnes-droites">X commentaires</td>
+	              </tr>
+	            </tbody>
+	          </table>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
 	</div>
 
-	<div id="description">
-		<?php
-			echo $userinfo['PRESENTATION'];
-		?>
-	</div>
-</body>
+
+	  
+
+	</body>
 </html>
+
 
 <?php
 }
