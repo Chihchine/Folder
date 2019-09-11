13b5f7dc87076a61a2f93b43d14c4c0157507ee0
TOMCO984 <tomco984@live.fr>
2019-09-11 22:01:02 +0200

    ajout classe utilisateur

---
+++ base/class/utilisateur.php
@@ -0,0 +1,13 @@
+<?php
+
+Class Utilisateur {
+
+  public static function Show($id) {
+    $request = Main::DataBase()->prepare("SELECT * FROM UTILISATEURS WHERE ID = ?");
+    $request->execute([$id]);
+    return $request->fetch();
+  }
+
+}
+
+?>
