6abe96687f620e0f3e61eb6a39aed1bb119e0f2d
AlexyDeforge <55129758+AlexyDeforge@users.noreply.github.com>
2019-09-11 17:16:38 +0200

    navbar

--- profil.php
+++ profil.php
@@ -165,7 +165,6 @@ require("base/include/header.php");
         </nav>
         <main class="page-content">
           <div class="container-fluid">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <!-- Contenu à droite -->
           </div>
 
