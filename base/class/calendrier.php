2425df1e69feb9e93350dc0b49810ea7a9056951
gauthiermairot <55086155+gauthiermairot@users.noreply.github.com>
2019-09-12 11:16:26 +0200

    fix

--- base/class/calendrier.php
+++ base/class/calendrier.php
@@ -19,16 +19,15 @@ Class Calendrier {
   }
 
   public static function eventCalendar() {
-      while ($$event = eventsDataBase()->fetch()) {
+      while ($event = eventsDataBase()->fetch()) {
         $dataEvent[] = array(
-          'title'     => $$event['NOM'],
-          'start'     => substr($$event['DATE_DEBUT'], 0, 10).'T'.substr($$event['DATE_DEBUT'], 11, 19),
-          'end'       => substr($$event['DATE_FIN'], 0, 10).'T'.substr($$event['DATE_FIN'], 11, 19),
+          'title'     => $event['NOM'],
+          'start'     => substr($event['DATE_DEBUT'], 0, 10).'T'.substr($event['DATE_DEBUT'], 11, 19),
+          'end'       => substr($event['DATE_FIN'], 0, 10).'T'.substr($event['DATE_FIN'], 11, 19),
           'color'     => '#2ca8ff'
         );
       }
   }
-  $dataEventJson = json_encode($dataEvent);
   return $dataEventJson;
 }
 
