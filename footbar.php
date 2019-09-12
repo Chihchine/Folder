8774e0379ebbec41cafeddee61abaf2f485ad954
gauthiermairot <55086155+gauthiermairot@users.noreply.github.com>
2019-09-12 15:10:32 +0200

    test

---
+++ footbar.php
@@ -0,0 +1,9 @@
+<?php require("base/include/header.php"); ?>
+
+
+<link rel="stylesheet" href="base/footer.css">
+<footer>
+  <div class="footbar">
+      <p>&copy; 2019 - My Campus</p>
+  </div>
+</footer>
