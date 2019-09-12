5889c8a347483a6826154109ecf68da3a687ccff
xXLgDGaMeRXx <loic.ducornetz@epsi.fr>
2019-09-12 13:51:48 +0200

    footer

--- base/include/footer.php
+++ base/include/footer.php
@@ -1,39 +1,39 @@
    <link href="base/css/footer.css" rel="stylesheet" id="css">
-        <footer class="site-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <h6>Informations</h6>
-                    <p class="text-justify"> Créé par COCHE Thomas, REYNARD Aurélien, MAIROT Gauthier, DEFORGE Alexy et DUCORNETZ Loïc pour le WorkShop de deuxième année.</p>
-                </div>
-            </div>
-            <hr>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-sm-6 col-xs-12">
-                    <p class="copyright-text">Copyright &copy; 2019 All Rights Reserved
-                    </p>
-                </div>
+   <footer class="site-footer">
+       <div class="container">
+           <div class="row">
+               <div class="col-sm-12 col-md-6">
+                   <h6>Informations</h6>
+                   <p class="text-justify"> Créé par COCHE Thomas, REYNARD Aurélien, MAIROT Gauthier, DEFORGE Alexy et DUCORNETZ Loïc pour le WorkShop de deuxième année.</p>
+               </div>
+           </div>
+           <hr>
+       </div>
+       <div class="container">
+           <div class="row">
+               <div class="col-md-8 col-sm-6 col-xs-12">
+                   <p class="copyright-text">Copyright &copy; 2019 All Rights Reserved
+                   </p>
+               </div>
 
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <ul class="social-icons">
-                        <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-                        <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
+               <div class="col-md-4 col-sm-6 col-xs-12">
+                   <ul class="social-icons">
+                       <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                       <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                       <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                       <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                   </ul>
+               </div>
+           </div>
+       </div>
+   </footer>
 
 
-    </div>
-    </main>
-    </div>
-    </body>
 
-    </html>
+
+   </div>
+   </main>
+   </div>
+   </body>
+
+   </html>
