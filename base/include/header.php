6aa716e5a4148a72dbe7438d796c27e4399c5646
AlexyDeforge <55129758+AlexyDeforge@users.noreply.github.com>
2019-09-12 15:48:51 +0200

    Test

--- base/include/header.php
+++ base/include/header.php
@@ -31,6 +31,7 @@ if (empty($imageProfil)) {
 	<link rel="stylesheet" href="<?php echo Settings::sitePathRoot; ?>base/css/vendor.bundle.addons.css">
 	<link rel="stylesheet" href="<?php echo Settings::sitePathRoot; ?>base/css/style.css">
 	<link rel="stylesheet" href="<?php echo Settings::sitePathRoot; ?>base/css/style0.css">
+	<link rel="stylesheet" href="<?php echo Settings::sitePathRoot; ?>base/css/bootstrap.min.css">
 	<link rel="shortcut icon" href="<?php echo Settings::sitePathRoot; ?>base/css/favicon.png">
 
 	<script src="<?php echo Settings::sitePathRoot; ?>base/js/jQuery.min.js"></script>
