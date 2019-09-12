c79fdcc0666222951a80490277f47abb2585f071
gauthiermairot <55086155+gauthiermairot@users.noreply.github.com>
2019-09-12 15:36:54 +0200

    ajout id dans la requête SQL annuaire

--- base/class/annuaire.php
+++ base/class/annuaire.php
@@ -1,7 +1,7 @@
 <?php
 Class Annuaire {
   public static function displayUsers() {
-    $request = Main::Database()->prepare('SELECT NOM, PRENOM, ECOLE, PROMOTION FROM UTILISATEURS');
+    $request = Main::Database()->prepare('SELECT ID, NOM, PRENOM, ECOLE, PROMOTION FROM UTILISATEURS');
     $request->execute();
     return $request;
   }
